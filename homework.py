ed10f500a71b2c2e91b7c5a706b973e542693231
firewaterr <2200011013@stu.pku.edu.cn>
2025-03-16 13:44:48 +0800

    changed function types

--- homework.py
+++ homework.py
@@ -5,27 +5,33 @@ def f(j, x, a = 1, b = 2, c = 3, d = 4):
     if j == 0:
         return np.sin(x)
     elif j == 1:
-        return a*x**2 + b*x + c
+        return a*x + b
     elif j == 2:
+        return a*x**2 + b*x + c
+    elif j == 3:
         return a*x**3 + b*x**2 + c*x + d
 
-def df(x, a = 1, b = 2, c = 3, d = 4):
+def df(j, x, a = 1, b = 2, c = 3, d = 4):
     if j == 0:
         return np.cos(x)
     elif j == 1:
-        return 2*a*x + b
+        return a
     elif j == 2:
+        return 2*a*x + b
+    elif j == 3:
         return 3*a*x**2 + 2*b*x + c
 
-def d2f(x, a = 1, b = 2, c = 3, d = 4):
+def d2f(j, x, a = 1, b = 2, c = 3, d = 4):
     if j == 0:
         return -np.sin(x)
     elif j == 1:
-        return 2*a
+        return 0
     elif j == 2:
+        return 2*a
+    elif j == 3:
         return 6*a*x + 2*b
 
-grid = np.linspace(0, 2*np.pi, 104)
+grid = np.linspace(0, 2*np.pi, 1004)
 f_values = np.zeros(len(grid))
 df_values = np.zeros((len(grid), 2))
 d2f_values = np.zeros((len(grid), 2))
@@ -34,7 +40,7 @@ d2f_real = np.zeros(len(grid))
 df_error = np.zeros(2) 
 d2f_error = np.zeros(2)
 
-for j in range (0, 3):
+for j in range (0, 4):
     for i in range (0, len(grid)): 
         f_values[i] = f(j, grid[i])
         df_real[i] = df(j, grid[i])
